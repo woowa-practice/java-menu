4fd020eccde69ebdab9c955dda9d94c9b64cbbae
snow___shower <wonsb25@gmail.com>
2026-01-04 16:12:43 +0900

    feat: 코치 리포지토리 작성

--- src/main/java/menu/domain/Coach.java
+++ src/main/java/menu/domain/Coach.java
@@ -1,14 +1,15 @@
 package menu.domain;
 
 import java.util.List;
+import java.util.Map;
 
 public class Coach {
     private final String name;
-    private String category;
-    private final String menu;
+    private final Map<String, Integer> category;
+    private final List<String> menu;
     private final List<String> banMenu;
 
-    public Coach(String name, String category, String menu, List<String> banMenu){
+    public Coach(String name, Map<String, Integer> category, List<String> menu, List<String> banMenu){
         this.name=name;
         this.category=category;
         this.menu=menu;
@@ -19,11 +20,11 @@ public class Coach {
         return name;
     }
 
-    public String getCategory(){
+    public Map<String, Integer> getCategory(){
         return category;
     }
 
-    public String getMenu() {
+    public List<String> getMenu() {
         return menu;
     }
 
---
+++ src/main/java/menu/repository/CoachRepository.java
@@ -0,0 +1,19 @@
+package menu.repository;
+
+import menu.domain.Category;
+import menu.domain.Coach;
+import menu.domain.Menu;
+
+import java.util.*;
+
+public class CoachRepository {
+    private static final List<Coach> coachs=new ArrayList<>();
+
+    public static List<Coach> getCoachs() {
+        return Collections.unmodifiableList(coachs);
+    }
+
+    public static void addCoach(Coach coach){
+        coachs.add(coach);
+    }
+}
